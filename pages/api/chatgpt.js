1f63d0438ece332a89b835cb3b1da644ad865146
liujianrong <704834659@qq.com>
2022-12-20 15:31:57 +0800

    feat: test api

---
+++ pages/api/chatgpt.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from "openai"
+
+
+const fn = async (req, res) => {
+	const configuration = new Configuration({
+		apiKey: process.env.OPENAI_API_KEY,
+	});
+	const openai = new OpenAIApi(configuration);
+
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: "你好",
+		temperature: 0.7,
+		max_tokens: 256,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
+	});
+	// res.status(200).end(JSON.stringify(response))
+	console.log('===response', response.data);
+	res.status(200).json(response.data.choices[0].text)
+}
+
+export default fn
+
+export const config = {
+	type: 'experimental-background',
+}
