00caba93729fd3c06d48b7fb8c4d97623e53861c
jianrong <704834659@qq.com>
2023-02-11 23:31:12 +0800

    test

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -5,11 +5,14 @@ const fn = async (req, res) => {
 	const configuration = new Configuration({
 		apiKey: process.env.OPENAI_API_KEY,
 	});
+
+	const { ask } = req.query
+
 	const openai = new OpenAIApi(configuration);
 
 	const response = await openai.createCompletion({
 		model: "text-davinci-003",
-		prompt: "你好",
+		prompt: ask,
 		temperature: 0.7,
 		max_tokens: 256,
 		top_p: 1,
